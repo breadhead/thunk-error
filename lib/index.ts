485494aad87f772a4d596be7bc04672c2999c0f6
Igor Kamyshev <igor@kamyshev.me>
2018-11-30 13:53:12 +0300

    Added code

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,42 @@
-console.log('Hello')
+import { Dispatch, Action, MiddlewareAPI } from 'redux'
+
+type SupportsError<Error> = (error: Error) => boolean
+type HandleError = () => Action | Function
+
+export const createErrorMiddleware = <Error = any>(
+  supportsError: SupportsError<Error>,
+  dispatchOnError: HandleError,
+) =>
+  (middleware: MiddlewareAPI<any>) =>
+    (next: Dispatch<any>) =>
+      (action: any) => next(
+        action instanceof Function
+          ? addErrorHandling(middleware, supportsError, dispatchOnError)(action)
+          : action,
+      )
+
+const addErrorHandling = <Error>(
+  middleware: MiddlewareAPI<any>,
+  supportsError: SupportsError<Error>,
+  dispatchOnError: HandleError,
+) =>
+  (fnAction: Function) =>
+    (...args: any[]) => {
+      const handleError = (err: Error) => {
+        if (supportsError(err)) {
+          middleware.dispatch(dispatchOnError())
+        }
+      }
+
+      let result
+
+      try {
+        result = fnAction(...args)
+      } catch (err) {
+        handleError(err) // sync error in reducer within a thunk
+      }
+
+      return result instanceof Promise
+        ? result.catch(handleError) // async error in thunk
+        : result
+    }
